750afc82de762ce43d0a054d7b11367b83e097b0
NaiChiehLin <daniellin456@gmail.com>
2021-06-01 19:56:44 +0800

    <add> remove comment in inner product

--- operator.cpp
+++ operator.cpp
@@ -3,11 +3,11 @@ double inner_product(double *a, double *b, int type, int N, int N_ln)
 {
         double kk = 0.0;
         if (type == 0){                         // for N_ln^2 * N_ln^2
-//#               pragma omp parallel for reduction(+:kk)
+#               pragma omp parallel for reduction(+:kk)
 		for (int i=0;i<N_ln*N_ln;i++) kk += a[i]*b[i];
         }
         else{                                   // for N^2 * N_ln^2
-//#               pragma omp parallel for reduction(+:kk)
+#               pragma omp parallel for reduction(+:kk)
 		for (int i=0;i<N_ln;i++)
                 for (int j=0;j<N_ln;j++){
                          kk += a[N*(i+1)+(j+1)]*b[N_ln*i+j];
