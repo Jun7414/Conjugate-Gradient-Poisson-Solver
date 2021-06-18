f47fc5106bd28b2a7237accc2b63573bddee0ca9
Jun7414 <eric08080@gmailcom>
2021-06-18 13:07:47 +0800

    <pull>

--- init.cpp
+++ init.cpp
@@ -73,13 +73,8 @@ void const_bc(double *u, double u0, int N)
         }
     }
 */
-<<<<<<< HEAD
    
 /*    // 3 side same 1 side different
-=======
-*   
-    // 3 side same 1 side different
->>>>>>> dev-daniel
     for (int i = 0; i < N; i++) // work
     {
         for (int j = 0; j < N; j++)
@@ -96,20 +91,11 @@ void const_bc(double *u, double u0, int N)
         }
     }
 */
-<<<<<<< HEAD
-    
-=======
-  
->>>>>>> dev-daniel
     for (int i = 0; i < N * N; i++) // Work
     {
         u[i] = u0;
     }
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> dev-daniel
     return;
 }
 
@@ -139,11 +125,8 @@ void point_source(double *d, int N_ln)
         }
     }
 */
-<<<<<<< HEAD
+
 /*    for( int i = 1; i < N_ln; i++ )
-=======
-    *for( int i = 1; i < N_ln; i++ )
->>>>>>> dev-daniel
     {
         for( int j = 0; j < N_ln; j++ )
         {
